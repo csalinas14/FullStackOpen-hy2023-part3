78e079f3b7c998abb464ed7314f42c82f8d04a25
Carlos Salinas <salinas.carlos14@gmail.com>
2023-08-07 14:17:22 -0400

    Updating backend

--- index.js
+++ index.js
@@ -62,29 +62,43 @@ app.get('/api/persons', (request, response) => {
   })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons_count} people</p>
-                    <p>${date}</p>`)
+    Person.find({}).then(persons => {
+        response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`)
+    })
+    
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
+    //const id = Number(request.params.id)
+    //const person = persons.find(person => person.id === id)
 
-    if(person){
-        response.json(person)
-    }else{
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person){
+                response.json(person)
+            }
+            else{
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    //const id = Number(request.params.id)
+    //persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+    
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const getRandomInt = (max) => {
         return Math.floor(Math.random() * max);
     }
@@ -96,23 +110,67 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+
     if(persons.find(p => p.name === body.name)){
         return response.status(400).json({
             error: 'name is not unique'
         })
     }
 
+    const person = new Person({
+        //id: getRandomInt(10000),
+        name: body.name,
+        number: body.number
+    })
+    
+
+    //persons = persons.concat(person)
+    console.log(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
+    
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
     const person = {
-        id: getRandomInt(10000),
         name: body.name,
         number: body.number
     }
-    
 
-    persons = persons.concat(person)
-    console.log(person)
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
+  
+  // this has to be the last loaded middleware.
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
